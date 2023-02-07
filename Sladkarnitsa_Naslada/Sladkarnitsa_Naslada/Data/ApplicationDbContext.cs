53087d38c0f185aa49b3a76732d2b3581f20f834
iZaharieva <i.zaharieva2018@pmg-pernik.com>
2023-02-07 17:53:30 +0200

    message

---
+++ Sladkarnitsa_Naslada/Sladkarnitsa_Naslada/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Sladkarnitsa_Naslada.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sladkarnitsa_Naslada.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Maker> Makers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
