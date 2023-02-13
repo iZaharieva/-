5423d272d107245925ec696c944cfe86d4044405
iZaharieva <i.zaharieva2018@pmg-pernik.com>
2023-02-13 19:17:44 +0200

    message

---
+++ SladkarnitsaNaslada/Sladkarnitsa_Naslada/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sladkarnitsa_Naslada.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        public string CategoryName { get; set; }
+
+        public virtual IEnumerable<Product> Products { get; set; } = new List<Product>();
+    }
+}
---
+++ SladkarnitsaNaslada/Sladkarnitsa_Naslada/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sladkarnitsa_Naslada.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
